53d142ea341f6b0b4df7921fc7c892b9aa5f162a
Mallikarjun <mallikarjunkamtam@gmail.com>
2022-04-25 08:03:46 +0530

    adding foundation files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const arr = [];
+  const [display, setDisply] = useState("");
+
+  function numberClicker(event) {
+    if (event.target.innerHTML == "Reset") {
+      setDisply("");
+    } else {
+      setDisply([...display, event.target.innerHTML]);
+      if (event.target.innerHTML != "=") {
+        arr.push(...display, event.target.innerHTML);
+      }
+      if (event.target.innerHTML == "=") {
+        setDisply(eval(display.join("")));
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="body">
+        <div className="header">
+          <h1 className="display">{display}</h1>
+        </div>
+        <div className="buttons">
+          <div className="numbers">
+            <div className="row1">
+              <button onClick={numberClicker} className="one">
+                1
+              </button>
+              <button onClick={numberClicker} className="two">
+                2
+              </button>
+              <button onClick={numberClicker} className="three">
+                3
+              </button>
+            </div>
+
+            <div className="row2">
+              <button onClick={numberClicker} className="four">
+                4
+              </button>
+              <button onClick={numberClicker} className="five">
+                5
+              </button>
+              <button onClick={numberClicker} className="six">
+                6
+              </button>
+            </div>
+            <div className="row3">
+              <button onClick={numberClicker} className="seven">
+                7
+              </button>
+              <button onClick={numberClicker} className="eight">
+                8
+              </button>
+              <button onClick={numberClicker} className="nine">
+                9
+              </button>
+            </div>
+            <div className="row4">
+              <button onClick={numberClicker} className="zero">
+                0
+              </button>
+            </div>
+          </div>
+          <div className="operators">
+            <button onClick={numberClicker} className="plus">
+              +
+            </button>
+            <button onClick={numberClicker} className="minus">
+              -
+            </button>
+            <button onClick={numberClicker} className="cross">
+              *
+            </button>
+            <button onClick={numberClicker} className="divide">
+              /
+            </button>
+            <button onClick={numberClicker} className="percentage">
+              %
+            </button>
+          </div>
+          <div className="result">
+            <button className="equal" onClick={numberClicker}>
+              Reset
+            </button>
+            <button className="equal" onClick={numberClicker}>
+              =
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
